470b88b845262c91adc712db1a3f9b9a300c4b49
apu242007 <jorge_e_castro@hotmail.com>
2026-02-13 09:27:59 -0300

    Fix: Añadida ruta base para que GitHub Pages funcione

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Tacker-Digital-Report-HTTAS2026/', // <--- AGREGA ESTA LÍNEA AQUÍ
       server: {
         port: 3000,
         host: '0.0.0.0',
